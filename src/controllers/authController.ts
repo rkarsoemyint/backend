e00f17329b99f7c2f028859f52c0e42b991434e6
Thant Zin Oo <tzoo2024@gmail.com>
2026-03-20 19:29:30 +0630

    authController.ts

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { User } from '../models/User'; 
+import User from '../models/User'; 
 
 export const login = async (req: Request, res: Response) => {
   try {
